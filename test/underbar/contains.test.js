b4a2e4cb94f595068272d4dfd294e057ecf593a8
ststar8623 <ststar8623@gmail.com>
2017-05-08 15:37:27 -0700

    underbar.contains

--- test/underbar/contains.test.js
+++ test/underbar/contains.test.js
@@ -3,18 +3,34 @@ const _ = require('../../underbar');
 describe('contains()', () => {
   it('returns true if an array contains the target', () => {
     // Your code goes here
+    expect(_.contains([1,2,3], 2)).toBe(true);
   });
 
   it('returns false if an array does not contain the target', () => {
     // Your code goes here
+    expect(_.contains([1,2,3], 5)).toBe(false);
   });
 
   it('returns true if the target value is among the values of an object', () => {
     // Your code goes here
+    const obj = {
+      '1': 1,
+      '2': 2,
+      '3': 3
+    };
+
+    expect(_.contains(obj, 3)).toBe(true);
   });
 
   it('returns false if the target value is not among the values of an object', () => {
     // Your code goes here
+    const obj = {
+      '1': 1,
+      '2': 2,
+      '3': 3
+    };
+
+    expect(_.contains(obj, 5)).toBe(false);
   });
 
 });
