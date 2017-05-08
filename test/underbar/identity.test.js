3f25bfc331e8dc5a7486ceb56a9d7d2a2b390b00
ststar8623 <ststar8623@gmail.com>
2017-05-08 14:18:07 -0700

    identity.test

--- test/underbar/identity.test.js
+++ test/underbar/identity.test.js
@@ -3,17 +3,27 @@ const _ = require('../../underbar');
 describe('identity()', () => {
   it('returns null if given null', () => {
     // Your code goes here
+    expect(_.identity(null)).toBe(null);
   });
 
   it('returns the number if given a number', () => {
     // Your code goes here
+    expect(_.identity(4)).toBe(4);
   });
 
   it('returns the same array if given an array', () => {
     // Your code goes here
+    const array = [1, 2, 3];
+    expect(_.identity(array)).toEqual(array);
   });
 
   it('returns the same object if given an object', () => {
     // Your code goes here
+    const object = {
+      1:1, 
+      'number': 3, 
+      'string': 'steven'
+    };
+    expect(_.identity(object)).toEqual(object);
   });
 });
