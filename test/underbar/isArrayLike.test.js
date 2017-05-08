dee39f314ac8a39ed69c0ad4e594848532f2375c
ststar8623 <ststar8623@gmail.com>
2017-05-08 15:55:06 -0700

    underbar.isArrayLike

--- test/underbar/isArrayLike.test.js
+++ test/underbar/isArrayLike.test.js
@@ -3,13 +3,25 @@ const _ = require('../../underbar');
 describe('isArrayLike()', () => {
   it('returns true for an actual array', () => {
     // Your code goes here
+    const arr = [1,2,3];
+    expect(_.isArrayLike(arr)).toBe(true);
   });
 
   it('returns true for an array-like object', () => {
     // Your code goes here
+    const arrLikeObj = {
+      length: 1
+    };
+    expect(_.isArrayLike(arrLikeObj)).toBe(true);
   });
 
   it('returns false for a non-array-like object', () => {
     // Your code goes here
+    const nonArrLikeObj = {
+      'first': 'steven',
+      'last': 'leung',
+      'age': 21
+    };
+    expect(_.isArrayLike(nonArrLikeObj)).toBe(false);
   });
 });
