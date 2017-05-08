005a4d5701705844ca7e761e611da6c4cfb7e721
ststar8623 <ststar8623@gmail.com>
2017-05-08 16:03:34 -0700

    underbar.reduce

--- test/underbar/reduce.test.js
+++ test/underbar/reduce.test.js
@@ -3,10 +3,14 @@ const _ = require('../../underbar');
 describe('reduce()', () => {
   it('reduces an array of numbers to a sum, *with* an explicit initial value for the accumulator', () => {
     // Your code goes here
+    const arr = [1,2,3,4,5];
+    expect(_.reduce(arr, (acc, num) => acc + num)).toBe(15);
   });
 
   it('reduces an array of numbers to a sum, *without* an explicit initial value for the accumulator', () => {
     // Your code goes here
+    const arr = [1,2,3,4,5];
+    expect(_.reduce(arr, (acc, num) => acc + num, 10)).toBe(25);
   });
 
 });
