c40fcd7cb5d4cc4a9145da52563b1ea8a9694d3c
ststar8623 <ststar8623@gmail.com>
2017-05-08 16:04:31 -0700

    underbar.uniq

--- test/underbar/uniq.test.js
+++ test/underbar/uniq.test.js
@@ -3,5 +3,7 @@ const _ = require('../../underbar');
 describe('uniq()', () => {
   it('de-dups a list of numbers', () => {
     // Your code goes here
+    const arr = [1,2,1,2,1,2];
+    expect(_.uniq(arr)).toEqual([1,2]);
   });
 });
