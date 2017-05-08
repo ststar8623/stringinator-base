f5c9ba09cb641fa2803d8fc5094f8d319df1ac4b
ststar8623 <ststar8623@gmail.com>
2017-05-08 15:56:28 -0700

    underbar.map

--- test/underbar/map.test.js
+++ test/underbar/map.test.js
@@ -3,5 +3,7 @@ const _ = require('../../underbar');
 describe('map()', () => {
   it('maps every numbers in an array of numbers to their square', () => {
     // Your code goes here
+    const arr = [1,2,3];
+    expect(_.map(arr, nums => nums * 2)).toEqual([2,4,6]);
   });
 });
