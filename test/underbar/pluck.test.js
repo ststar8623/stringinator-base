289c7a5b844e00d7fcb6343d4d6deb595cdecdca
ststar8623 <ststar8623@gmail.com>
2017-05-08 16:00:40 -0700

    underbar.pluck

--- test/underbar/pluck.test.js
+++ test/underbar/pluck.test.js
@@ -3,6 +3,13 @@ const _ = require('../../underbar');
 describe('pluck()', () => {
   it('returns an array of just ages, given an array of people objects', () => {
     // Your code goes here
+    const peopleObj = [
+      { name: 'steven', age: 21 },
+      { name: 'andy', age: 31 },
+      { name: 'mom', age: 55 }
+    ];
+
+    expect(_.pluck(peopleObj, 'age')).toEqual([21,31,55]);
   });
 
 });
